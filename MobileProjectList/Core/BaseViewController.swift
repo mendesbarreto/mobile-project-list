bea9030ebe5a785c54456d6adf50c9d12a330b23
Douglas Mendes <mendes-barreto@live.com>
2018-04-15 19:51:00 -0300

    Add loading screen to project and a base view controller

---
+++ MobileProjectList/Core/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+// Created by Douglas Mendes Barreto. on 4/15/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    private var loadingAlert: UIAlertController?
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        print("this class can not be initialized for NSCoder arg")
+        return nil
+    }
+}
+
+extension BaseViewController: LoadingPresenter {
+    func showLoading() {
+        loadingAlert = LoadingAlertScreenFactory.make()
+        present(loadingAlert!, animated: true)
+    }
+
+    func hideLoading(onComplete: (() -> Void)?) {
+        loadingAlert?.dismiss(animated: true) { [weak self] in
+            onComplete?()
+            self?.loadingAlert = nil
+        }
+    }
+}
---
+++ MobileProjectList/Core/Strings.swift
@@ -0,0 +1,17 @@
+//
+// Created by douglas.barreto on 4/15/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import Foundation
+
+struct Strings {
+
+    struct App {
+        static let baseUrl = ""
+    }
+
+    struct LoadingAlert {
+        static let message = "Por favor espere..."
+    }
+}
