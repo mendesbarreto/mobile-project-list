e51f77f7528ae5ee4324192d590b9d1c9f58acd2
Douglas Mendes <mendes-barreto@live.com>
2018-04-22 17:11:30 -0300

    Add factory to build and delivery default system alerts

---
+++ MobileProjectList/Core/Factory/AlertScreenFactory.swift
@@ -0,0 +1,18 @@
+//
+// Created by douglas.barreto on 4/22/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import UIKit
+
+final class AlertScreenFactory {
+    static func make(viewModel: AlertViewModel) -> UIAlertController {
+        let alertViewController = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: viewModel.style)
+
+        viewModel.actions.forEach { action in
+            alertViewController.addAction(action)
+        }
+
+        return alertViewController
+    }
+}
